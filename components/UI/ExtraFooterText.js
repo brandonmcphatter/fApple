d4d98c48f20a31f624e9f563df5bf35d07bf56a7
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-10-27 12:19:07 -0400

    ExtraFooterText component

---
+++ components/UI/ExtraFooterText.js
@@ -0,0 +1,20 @@
+export default function ExtraFooterText() {
+    return (
+        /*More Footer Text*/
+    <ul className={'pt-4 tracking-wide grid gap-3'}
+        style={{fontFamily: 'Helvetica', fontSize: '12px', color: '#6b7280'}}>
+        <li>
+            More ways to shop: Find an Apple Store or other retailer near you. Or call 1-800-MY-APPLE.
+        </li>
+        <li>
+            United States
+        </li>
+        <li>
+            Copyright © 2024 Apple Inc. All rights reserved.
+        </li>
+        <li>
+            Privacy Policy | Terms of Use | Sales and Refunds | Legal | Site Map
+        </li>
+    </ul>
+    )
+}
