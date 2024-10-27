8a2393752861d49921dbe516d1e215b839685610
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-10-27 11:31:26 -0400

    initial

--- app/page.js
+++ app/page.js
@@ -1,101 +1,92 @@
 import Image from "next/image";
+import iPhone16Pro from '/public/img/iphone 16 pro.jpg';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <>
+      <main className={' '}>
+          <FeatureTile product={'iPhone 16 Pro'} />
+          <ContentTile product={'iPhone 16'}/>
+          <ContentTile product={'Apple Watch'}/>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </>
   );
+}
+
+function FeatureTile({product}) {
+    return (
+        <div className={'w-full h-1/5 md:h-2/3 bg-black text-white flex justify-center mb-3 pb-10'}>
+            <div className={'flex-col items-stretch w-1/2'}>
+                <div className={'mt-20 mb-60 z-10'}>
+                    {/*Product Title*/}
+                    <div className={'text-center'}>
+                          <span className={''} style={{fontFamily: 'Helvetica', fontSize: '32px'}}>
+                              {product}
+                          </span>
+                    </div>
+                    {/*Product Quote*/}
+                    <div className={'text-center'} style={{fontFamily: 'Helvetica', fontSize: '16px'}}>
+                          <span>
+                              Hello, Apple Intelligence.
+                          </span>
+                    </div>
+                </div>
+
+                {/*Content Split*/}
+
+                <div className={'self-end'}>
+                    {/*Buttons*/}
+                    <div className={'flex justify-center gap-3 mb-4'}>
+                        <button className={'bg-blue-500 text-white py-2 px-4 rounded-full text-sm'}>Learn More</button>
+                        <button className={'bg-transparent text-blue-500 border border-blue-500 py-2 px-4 rounded-full text-sm'}>Buy</button>
+
+                    </div>
+                    {/*Small Text*/}
+                    <div className={'text-center'}>
+                        <span className={'text-gray-500'}>Apple Intelligence coming this fall</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function ContentTile({product}) {
+    return (
+        <div className={'w-full h-1/2 md:h-5/6 bg-purple-600 text-white flex justify-center mb-3'}>
+            <div className={'flex-col items-stretch w-1/2'}>
+
+                <div className={'mt-20 mb-60'}>
+                    {/*Product Title*/}
+                    <div className={'text-center'}>
+                          <span className={''} style={{fontFamily: 'Helvetica', fontSize: '32px'}}>
+                              {product}
+                          </span>
+                    </div>
+                    {/*Product Quote*/}
+                    <div className={'text-center'} style={{fontFamily: 'Helvetica', fontSize: '16px'}}>
+                          <span className={''}>
+                              Hello, Apple Intelligence.
+                          </span>
+                    </div>
+                </div>
+
+                {/*Content Split*/}
+
+                <div className={'self-end'}>
+                    {/*Buttons*/}
+                    <div className={'flex justify-center gap-3 mb-4'}>
+                        <button className={'bg-blue-500 text-white py-2 px-4 rounded-full text-sm'}>Learn More</button>
+                        <button className={'bg-transparent text-blue-500 border border-blue-500 py-2 px-4 rounded-full text-sm'}>Buy</button>
+
+                    </div>
+                    {/*Small Text*/}
+                    <div className={'text-center'}>
+                        <span className={'text-black'}>Apple Intelligence coming this fall</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
