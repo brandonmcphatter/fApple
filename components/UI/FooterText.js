cac3065526e5ef2561817aae02459e6754f70c38
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-10-27 12:18:44 -0400

    FooterText component

---
+++ components/UI/FooterText.js
@@ -0,0 +1,62 @@
+export default function FooterText() {
+    return (
+        /*Footer Text*/
+    <ul className={'pt-4 tracking-wide grid gap-2'}
+        style={{fontFamily: 'Helvetica', fontSize: '12px', color: '#6b7280'}}>
+        <li>
+            1. Apple Intelligence will be available in beta on all iPhone 16 models, iPhone 15 Pro, iPhone
+            15 Pro Max, iPad mini (A17 Pro), and iPad and Mac models with M1 and later, with Siri and device
+            language set to U.S. English, as part of an iOS 18, iPadOS 18, and macOS Sequoia update this
+            fall. English (Australia, Canada, New Zealand, South Africa, UK) language support available this
+            December. Some features, additional platforms, and support for additional languages, like
+            Chinese, English (India, Singapore), French, German, Italian, Japanese, Korean, Portuguese,
+            Spanish, Vietnamese, and others, will be coming over the course of the next year.
+        </li>
+        <li>
+            2. Available in two models: AirPods 4 and AirPods 4 with Active Noise Cancellation.
+        </li>
+        <li>
+            3. Hearing Test and Hearing Aid features expected fall 2024. They will be supported on AirPods Pro 2
+            with the latest firmware paired with a compatible iPhone or iPad with iOS 18 or iPadOS 18 and later,
+            and are intended for people 18 years old or older. The Hearing Aid feature will be intended for
+            people with perceived mild to moderate hearing loss and is pending FDA review.
+        </li>
+        <li>
+            4. Trade-in values will vary based on the condition, year, and configuration of your eligible
+            trade-in device. Not all devices are eligible for credit. You must be at least 18 years old to be
+            eligible to trade in for credit or for an Apple Gift Card. Trade-in value may be applied toward
+            qualifying new device purchase, or added to an Apple Gift Card. Actual value awarded is based on
+            receipt of a qualifying device matching the description provided when estimate was made. Sales tax
+            may be assessed on full value of a new device purchase. In-store trade-in requires presentation of a
+            valid photo ID (local law may require saving this information). Offer may not be available in all
+            stores, and may vary between in-store and online trade-in. Some stores may have additional
+            requirements. Apple or its trade-in partners reserve the right to refuse or limit quantity of any
+            trade-in transaction for any reason. More details are available from Apple’s trade-in partner for
+            trade-in and recycling of eligible devices. Restrictions and limitations may apply.
+        </li>
+        <li>
+            To access and use all Apple Card features and products available only to Apple Card users, you must
+            add Apple Card to Wallet on an iPhone or iPad that supports and has the latest version of iOS or
+            iPadOS. Apple Card is subject to credit approval, available only for qualifying applicants in the
+            United States, and issued by Goldman Sachs Bank USA, Salt Lake City Branch.
+        </li>
+        <li>
+            If you reside in the U.S. territories, please call Goldman Sachs at 877-255-5923 with questions
+            about Apple Card.
+        </li>
+        <li>
+            Learn more about how Apple Card applications are evaluated at support.apple.com/kb/HT209218.
+        </li>
+        <li>
+            A subscription is required for Apple TV+.
+        </li>
+        <li>
+            A subscription is required for Apple Arcade, Apple Fitness+, and Apple Music.
+        </li>
+        <li>
+            Features are subject to change. Some features, applications, and services may not be available in
+            all regions or all languages.
+        </li>
+    </ul>
+    )
+}
