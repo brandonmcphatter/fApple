a444e3cf6b23e09c030dde75fb6e54345ce60545
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-10-27 12:40:08 -0400

    Changed opacity and backdropfilter to properly change on scroll

--- components/UI/NavBar.js
+++ components/UI/NavBar.js
@@ -5,16 +5,16 @@ import {faGripLines, faShoppingBag} from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
     return (
-        <nav style={{backgroundColor: '#181818', opacity: '95%'}} className={'fixed w-full'}>
+        <nav style={{backgroundColor: 'rgba(24,24,24,0.68)', opacity: '99%', backdropFilter: 'blur(30px)'}} className={'fixed w-full'}>
             <div className={'h-12 md:h-11 flex px-4 md:justify-evenly content-center lg:w-1/2 lg:mx-auto'}>
                 {/*Apple Logo*/}
                 <div className={'w-4 md:w-4 text-white content-center me-auto md:ms-1'}>
-                    <FontAwesomeIcon icon={faApple} style={{color: '#afafaf'}}/>
+                    <FontAwesomeIcon icon={faApple} style={{color: 'white'}}/>
                 </div>
                 {/*Product List*/}
                 <div className={'content-center hidden md:inline mx-auto px-2 w-full'}>
                     <ul className={'flex gap-3 justify-evenly'}
-                        style={{color: '#afafaf', fontFamily: 'Helvetica', fontSize: '12px'}}>
+                        style={{color: 'white', fontFamily: 'Helvetica', fontSize: '12px'}}>
                         <li>Store</li>
                         <li>Mac</li>
                         <li>iPad</li>
@@ -31,13 +31,13 @@ export default function NavBar() {
                 {/*Search and Bag*/}
                 <div className={'flex gap-8 md:gap-6 md:mx-1 content-center'}>
                     <div className={'min-w-4 text-white content-center'}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: '#afafaf'}}/>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: 'white'}}/>
                     </div>
                     <div className={'min-w-4 text-white content-center'}>
-                        <FontAwesomeIcon icon={faShoppingBag} style={{color: '#afafaf'}}/>
+                        <FontAwesomeIcon icon={faShoppingBag} style={{color: 'white'}}/>
                     </div>
                     <div className={'min-w-4 text-white content-center md:hidden'}>
-                        <FontAwesomeIcon icon={faGripLines} style={{color: '#afafaf'}}/>
+                        <FontAwesomeIcon icon={faGripLines} style={{color: 'white'}}/>
                     </div>
                 </div>
             </div>
