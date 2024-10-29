7be89906e23b8bfa051065d63fe43bd9ca7bdd85
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-10-29 16:38:14 -0400

    section for main featured products

---
+++ components/UI/FeaturedSection.js
@@ -0,0 +1,13 @@
+import IPhone16ProTile from "@/components/FeaturedContent/MainSection/iPhone16ProTile";
+import IPhone16Tile from "@/components/FeaturedContent/MainSection/iPhone16Tile";
+import AppleWatchUltraTile from "@/components/FeaturedContent/MainSection/AppleWatchUltraTile";
+
+export default function FeaturedSection() {
+    return (
+        <>
+            <IPhone16ProTile />
+            <IPhone16Tile />
+            <AppleWatchUltraTile />
+        </>
+    )
+}
