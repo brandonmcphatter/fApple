9fc1ad24ca5477994587d8c67db5de90c1bd4821
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-10-30 22:32:19 -0400

    iPhone 16 pro highlight carousel items

---
+++ components/iPhone16Pro/HighlightItem.js
@@ -0,0 +1,18 @@
+import styles from './HighlightItem.module.css';
+
+export default function HighlightItem({category, quote, btnText}) {
+    return (
+
+        <div className={styles.item}>
+
+            {/*Text and Button*/}
+            {/*<div className={'flex-col text-center pb-5'}>*/}
+            {/*    <div className={'font-bold text-sm '}>{category}</div>*/}
+            {/*    <div className={'text-sm px-4'}>{quote}</div>*/}
+            {/*    <button className={'bg-gray-300 hover:bg-gray-200 rounded-full mt-2 px-4 py-2 text-sm font-thin tracking-wide'}>*/}
+            {/*        {`${btnText === ''}` ? 'Stream Now' : btnText}*/}
+            {/*    </button>*/}
+            {/*</div>*/}
+        </div>
+    )
+}
