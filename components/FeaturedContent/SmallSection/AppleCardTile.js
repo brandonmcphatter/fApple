cd6886def7334a51cd5fd1ed6432a8f25c7e6348
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-03 22:32:55 -0500

    refactored to include image

--- components/FeaturedContent/SmallSection/AppleCardTile.js
+++ components/FeaturedContent/SmallSection/AppleCardTile.js
@@ -1,42 +1,43 @@
-import styles from "@/components/FeaturedContent/SmallSection/AirPodProTile.module.css";
+import styles from "@/components/FeaturedContent/SmallSection/AppleCardTile.module.css";
+import Image from "next/image";
+import appleCard from '/public/img/appleCard.png';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faApple} from "@fortawesome/free-brands-svg-icons";
 
 export default function AppleCardTile() {
-    const appleCardQuote = 'Get up to 3% Daily Cash Back with every purchase.';
     const product = 'Card';
-    const bgColor = '#f5f5f7';
-
     return (
-        <div className={styles.miniContent} style={{backgroundColor: `${bgColor}`}}>
-            <div className={'flex-col w-1/2 md:w-3/4 mt-30 md:mt-1 lg:mt-3'}>
-                <div className={'pt-10 mb-4 text-black'}>
+        <div className={styles.miniContent}>
+            <Image src={appleCard} alt={'AirPods Pro 2'} className={styles.img} />
+            <div className={'text-black flex-col mx-auto w-1/2 md:w-3/4 lg:w-1/2 pt-8 pb-5'}>
+                <div className={' '}>
                     {/*Product Title*/}
-                    <div className={'text-center'}>
+                    <div className={'text-center '}>
                           <span className={''} style={{fontFamily: 'Helvetica', fontSize: '32px'}}>
-                             <FontAwesomeIcon icon={faApple} style={{color: 'black'}}/>{product}
+                              <FontAwesomeIcon icon={faApple} />{product}
+                          </span>
+                    </div>
+                    <div className={'text-center -mt-2'} style={{fontFamily: 'Helvetica', fontSize: '16px'}}>
+                          <span className={'px-6 text-sm font-semibold text-center tracking-wide'}>
+                              SERIES 10
                           </span>
                     </div>
                     {/*Product Quote*/}
                     <div className={'text-center'} style={{fontFamily: 'Helvetica', fontSize: '16px'}}>
-                          <span className={'px-6 text-center'}>
-                              {appleCardQuote}
+                          <span className={'px-6 text-center  tracking-wide'}>
+                              Get up to 3% Daily Cash Back with every purchase.
                           </span>
                     </div>
                 </div>
 
-                <div className={''}>
+                <div className={'mt-3'}>
                     {/*Buttons*/}
                     <div className={'flex justify-center gap-3'}>
                         <button className={'bg-blue-500 text-white py-2 px-4 rounded-full text-sm'}>Learn More</button>
-                        <button className={'bg-transparent text-blue-500 border border-blue-500 py-2 px-4 rounded-full text-sm'}>Apply Now</button>
-
+                        <button className={'bg-transparent text-blue-500 border border-blue-500 py-2 px-4 rounded-full text-sm'}>Buy</button>
                     </div>
-                    {/*Small Text*/}
-                    {/*<div className={'text-center'}>*/}
-                    {/*    <span className={'text-black'}>Apple Intelligence coming this fall</span>*/}
-                    {/*</div>*/}
                 </div>
             </div>
-        </div>    )
+        </div>
+    )
 }
