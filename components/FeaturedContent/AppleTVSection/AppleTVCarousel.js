b50eafd99920f8b47784dc0cbd7fb0056b321072
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-04 14:24:47 -0500

    imported images and refactored

--- components/FeaturedContent/AppleTVSection/AppleTVCarousel.js
+++ components/FeaturedContent/AppleTVSection/AppleTVCarousel.js
@@ -2,8 +2,12 @@
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircle} from "@fortawesome/free-solid-svg-icons";
-import {useState} from "react";
 import CarouselItem from "@/components/FeaturedContent/AppleTVSection/CarouselItem";
+import badMonkey from '@/public/img/appleTV/badMonkeySmall.jpg';
+import silo from '@/public/img/appleTV/siloSmall.jpg';
+import loot from '@/public/img/appleTV/lootSmall.jpg';
+import severance from '@/public/img/appleTV/severanceSmall.jpg';
+import servant from '@/public/img/appleTV/servantSmall.jpg';
 
 export default function AppleTVCarousel() {
     const gry = 'gray';
@@ -13,13 +17,14 @@ export default function AppleTVCarousel() {
 
         <div style={{fontFamily: 'Helvetica'}}>
             {/*Carousel*/}
-            <div className={'flex  gap-4  overflow-x-auto'}>
-                <CarouselItem category={'Thriller'} quote={'Your past can drag you under.'}/>
-                <CarouselItem category={'Animation'} quote={'Happiness is a Halloween tradition.'}/>
-                <CarouselItem category={'Action'} quote={'George Clooney and Brad Pitt are rival fixers stuck on the same job for one wild night.'}/>
-                <CarouselItem category={'Comedy'} quote={'The shady side of paradise.'}/>
-                <CarouselItem category={'Thriller'} quote={'Emmy Award winner.'}/>
+            <div className={'flex ps-3 pe-8 gap-4  overflow-x-auto'}>
+                <CarouselItem imageSM={badMonkey}/>
+                <CarouselItem imageSM={silo}/>
+                <CarouselItem imageSM={loot}/>
+                <CarouselItem imageSM={severance}/>
+                <CarouselItem imageSM={servant}/>
             </div>
+
             {/*Carousel Dots*/}
             <div className={'flex justify-center content-center gap-3.5 my-2'}>
                     <FontAwesomeIcon icon={faCircle} size={"sm"} width={8} color={gry}/>
