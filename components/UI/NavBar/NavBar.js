0d765842c3b3d2c904245a853dd8891e058371f9
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-04 14:28:04 -0500

    linked home and iPhone

--- components/UI/NavBar/NavBar.js
+++ components/UI/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faApple} from "@fortawesome/free-brands-svg-icons";
 import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
 import {faGripLines, faShoppingBag} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 export default function NavBar() {
     return (
@@ -12,7 +13,9 @@ export default function NavBar() {
             <div className={'h-12 md:h-11 flex px-4 md:justify-evenly content-center lg:w-1/2 lg:mx-auto'}>
                 {/*Apple Logo*/}
                 <div className={'w-5 md:w-4 text-white content-center me-auto md:ms-1'}>
-                    <FontAwesomeIcon icon={faApple} style={{color: 'white'}}/>
+                    <Link href={'/'} >
+                        <FontAwesomeIcon icon={faApple} style={{color: 'white'}}/>
+                    </Link>
                 </div>
                 {/*Product List*/}
                 <div className={'content-center hidden md:inline-block mx-auto px-2 w-full'}>
@@ -21,7 +24,9 @@ export default function NavBar() {
                         <li>Store</li>
                         <li>Mac</li>
                         <li>iPad</li>
-                        <li>iPhone</li>
+                        <Link href={'/iphone-16-pro'} >
+                            <li>iPhone</li>
+                        </Link>
                         <li>Watch</li>
                         <li>Vision</li>
                         <li>AirPods</li>
