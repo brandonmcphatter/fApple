891657bc948e67ecead14af961146dca87ede78b
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-04 14:26:15 -0500

    refactored to include image and hover functionality

--- components/FeaturedContent/AppleTVSection/CarouselItem.js
+++ components/FeaturedContent/AppleTVSection/CarouselItem.js
@@ -1,18 +1,24 @@
 import styles from './CarouselItem.module.css';
+import Image from "next/image";
+import {useState} from "react";
 
-export default function CarouselItem({category, quote, btnText}) {
+export default function CarouselItem({imageSM, imageLG}) {
+    const [hidden, setHidden] = useState('hidden');
+    const [blur, setBlur] = useState('')
+    const blurred = ()=> setBlur('blur-sm');
+    const unBlur = ()=> setBlur('');
+    const unHide = () => setHidden('flex');
+    const hide = () => setHidden('hidden');
+
     return (
 
-        <div className={styles.item}>
+        <div className={styles.item} onMouseEnter={unHide} onMouseLeave={hide}>
+            <Image src={imageSM} alt={'small image'} objectFit={'cover'} className={blur}/>
+            <div className={`${hidden} w-full  justify-center content-center mx-auto bg-transparent absolute opacity-80 `}>
+                <button onMouseEnter={blurred} onMouseLeave={unBlur}  className={'bg-blue-500 hover:bg-blue-400 text-white rounded-full px-4 py-2 text-sm'}>Watch Now
+                </button>
+            </div>
 
-            {/*Text and Button*/}
-            {/*<div className={'flex-col text-center pb-5'}>*/}
-            {/*    <div className={'font-bold text-sm '}>{category}</div>*/}
-            {/*    <div className={'text-sm px-4'}>{quote}</div>*/}
-            {/*    <button className={'bg-gray-300 hover:bg-gray-200 rounded-full mt-2 px-4 py-2 text-sm font-thin tracking-wide'}>*/}
-            {/*        {`${btnText === ''}` ? 'Stream Now' : btnText}*/}
-            {/*    </button>*/}
-            {/*</div>*/}
         </div>
     )
 }
