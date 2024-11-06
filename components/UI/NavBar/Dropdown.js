ce12d261a69073519aace98aed1900408ca6bffd
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-06 14:07:05 -0500

    added transitions through views

--- components/UI/NavBar/Dropdown.js
+++ components/UI/NavBar/Dropdown.js
@@ -2,6 +2,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleLeft, faAngleRight, faX} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import {useState} from "react";
+import 'animate.css';
 
 export default function Dropdown({toggleMenu}) {
     const [view, setView] = useState(1);
@@ -9,7 +10,7 @@ export default function Dropdown({toggleMenu}) {
     const iPhoneView = ()=> setView(2);
 
     return (
-        <div className={'w-svw h-svh pt-3'} style={{backgroundColor: '#161617'}}>
+        <div className={'w-svw h-svh pt-3 animate__animated animate__slideInDown'} style={{backgroundColor: '#161617'}}>
             {/*Display Area*/}
             {view === 1 && <AllProducts iPhoneView={iPhoneView} toggleMenu={toggleMenu}/>}
             {view === 2 && <OptionView toggleMenu={toggleMenu} allView={allView} />}
@@ -19,18 +20,18 @@ export default function Dropdown({toggleMenu}) {
 
 function AllProducts({iPhoneView, toggleMenu}) {
     return (
-        <div>
+        <div className={'animate__animated animate__fadeIn animate__delay'}>
             {/*X and < buttons*/}
             <div className={'flex justify-end pt-5 pe-5 pb-8'}>
                 <FontAwesomeIcon onClick={toggleMenu} icon={faX} style={{color: '#e8e8ed'}} className={'cursor-pointer'}/>
             </div>
             {/*List of options*/}
-            <div className={'ps-10  text-2xl tracking-wide'} style={{fontFamily: 'Helvetica', color: '#e8e8ed'}}>
+            <div className={'ps-10  text-2xl tracking-wide animate__animated animate__slideInLeft'} style={{fontFamily: 'Helvetica', color: '#e8e8ed'}}>
                 <ul className={'grid'}>
                     <DropItem name={'Store'}/>
                     <DropItem name={'Mac'}/>
                     <DropItem name={'iPad'}/>
-                    <div onClick={iPhoneView}><DropItem name={'iPhone'}/></div>
+                    <div className={'animate__animated animate__pulse animate__delay-1s'} onClick={iPhoneView}><DropItem name={'iPhone'}/></div>
                     <DropItem name={'Watch'}/>
                     <DropItem name={'Vision'}/>
                     <DropItem name={'AirPods'}/>
@@ -46,7 +47,7 @@ function AllProducts({iPhoneView, toggleMenu}) {
 
 function OptionView({toggleMenu, allView}) {
     return (
-        <div>
+        <div className={'animate__animated animate__fadeIn'}>
             {/*X and < buttons*/}
             <div className={'flex justify-between pt-5 px-5 pb-8'}>
                 <FontAwesomeIcon onClick={allView} icon={faAngleLeft} style={{color: 'e8e8ed'}} className={'cursor-pointer'}/>
@@ -57,7 +58,7 @@ function OptionView({toggleMenu, allView}) {
                 <ul className={'grid'}>
                     <DropItem name={'Explore All iPhone'}/>
                     <Link onClick={toggleMenu} href={'/iphone-16-pro'}>
-                        <DropItem name={'iPhone 16 Pro'}/>
+                        <div className={'animate__animated animate__pulse delay-1s'}><DropItem name={'iPhone 16 Pro'}/></div>
                     </Link>
                     <DropItem name={'iPhone 16'}/>
                     <DropItem name={'iPhone 15'}/>
