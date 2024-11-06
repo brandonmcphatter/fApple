6ffb7db0589b42949f667d1b041f707309966f4d
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-06 12:51:51 -0500

    custom tile for product page

---
+++ components/iPhone16Pro/iPhoneTile.js
@@ -0,0 +1,49 @@
+import styles from "@/components/FeaturedContent/MainSection/iPhone16ProTile.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import iPhone16Pro from "@/public/img/featureIphonePhoto.jpg";
+
+export default function IPhoneTile() {
+    const product = 'iPhone 16 Pro';
+    const iPhoneQuote = 'Hello, Apple Intelligence.';
+
+    return (
+        <>
+            <div className={'h-12 lg:h-4 bg-black'}></div>
+            <div className={styles.iPhone16Pro}>
+                <Image src={iPhone16Pro} alt={'iphone 16'}
+                       className={styles.img}/>
+                <div className={''}>
+                    <div className={'pt-10 mb-4'}>
+                        {/*Product Title*/}
+                        <div className={'text-center'}>
+                                      <span className={''} style={{fontFamily: 'Helvetica', fontSize: '32px', fontWeight: 500}}>
+                                          {product}
+                                      </span>
+                        </div>
+                        {/*Product Quote*/}
+                        <div className={'text-center'} style={{fontFamily: 'Helvetica', fontSize: '16px'}}>
+                                      <span className={'px-6 text-center tracking-wide font-thin'}>
+                                          {iPhoneQuote}
+                                      </span>
+                        </div>
+                    </div>
+                    {/*Buttons*/}
+                    <div className={'flex justify-center gap-3'}>
+                        <Link href={'/iphone-16-pro'} >
+                            <button
+                                className={'bg-transparent hover:bg-blue-400 text-blue-500 hover:text-white border border-blue-500 py-2 px-4 rounded-full text-sm'}>Buy
+                            </button>
+                        </Link>
+
+
+                    </div>
+                    {/*Small Text*/}
+                    <div className={'text-center'}>
+                        <span className={'text-black'}>Apple Intelligence coming this fall</span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
